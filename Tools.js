51d7d01c3283de2653913e1fa700b2775e1e311f
a_boy <1471047476@qq.com>
2018-04-15 22:45:56 +0800

    添加一些工具函数

--- Tools.js
+++ Tools.js
@@ -1,6 +1,6 @@
 (function() {
 /**
- * js的工具库,方便使用
+ * JavaScript的工具库,方便使用
  * author   a_boy
  * created  2018-4-7 18:06
  * 
@@ -8,6 +8,7 @@
 	var Tools = {
 		/**
 		 * 日期时间换成Unix时间戳
+		 * data  	时间戳    必填
 		 * @param {[type]} data [description]
 		 */
 		DateToTimestamp: function(data) {
@@ -15,8 +16,11 @@
 		},
 		/**
 		 * Unix时间戳转成时间
-		 * @param {[type]} format    '格式: yy-MM-dd hh:mm:ss'
-		 * @param {[type]} timestamp '时间戳: 1525147931'
+		 * @param {[type]} format     格式: 	 			必填
+		 * 如: YY/MM/DD hh:mm:ss   
+		 *     YY年MM月DD日 hh时mm分ss秒
+		 *     Y-M-D h:m:s
+		 * @param {[type]} timestamp '时间戳: 1525147931'	可填(默认当前时间)
 		 */
 		TimestampToDate: function(format, timestamp) {
 			var date = timestamp ? new Date(parseInt(timestamp) * 1000) : new Date(+new Date());
@@ -66,8 +70,9 @@
 		},
 
 		/**
-		 * 普通数值转成两位小数
-		 * @param {[type]} money [description]
+		 * 精确到两位小数
+		 * @param {[type]} money 数值         必填
+		 * @param {[type]} num   精确多少位   可填(默认两位)
 		 */
 		ToCurrency: function(money, num = 2) {
 			return parseFloat(money).toFixed(num);
@@ -75,6 +80,7 @@
 
 		/**
 		 * 获取url地址参数
+		 * param    获取某一个参数    可填(默认返回所有参数)
 		 * @return {[type]} [description]
 		 */
 		getUrlParmas: function(param) {
@@ -97,23 +103,73 @@
 		},
 		/**
 		 * 设置cookies
+		 * @param {[type]} name    cookie名称    必填
+		 * @param {[type]} value   cookie值		 必填
+		 * @param {[type]} expires 缓存多少秒	 必填
+		 * @param {[type]} options 对象          可填(默认设置根目录)
 		 */
-		setCookies: function(name, value, expires) {
+		setCookies: function(name, value, expires, options = {}) {
 			var cookieText = name + '=' + value;
+			options.path = options.path || '/';
 			if(expires) {
 				var timestamp = (new Date().getTime()) / 1000 + expires;
 				var dateObj = new Date(timestamp * 1000);
-				// console.log(timestamp, dateObj)
 				cookieText += '; expires=' + dateObj.toGMTString();
 			}
-			console.log(cookieText)
+
+			if(options.path) {
+				cookieText += '; path=' + options.path;
+			}
+			if(options.domain) {
+				cookieText += '; domain=' + options.domain;
+			}
 			document.cookie = cookieText;
+		},
+		/**
+		 * 获取cookie的值
+		 * @param  {[type]} name cookie值
+		 * @return {[type]}      [description]
+		 */
+		getCookie: function(name) {
+			var value = document.cookie,
+				cookieName = name + '=';
+				cookieStart = value.indexOf(cookieName),
+				cookieValue = null;
+			if(cookieStart > -1) {
+				var cookieEnd = value.indexOf(';', cookieStart);
+				cookieEnd = cookieEnd > -1 ? cookieEnd : value.length;
+				cookieValue = value.substring(cookieStart + cookieName.length, cookieEnd)
+			}
+			return cookieValue;
 		},
 		/**
 		 * 清除cookies
 		 * @return {[type]} [description]
 		 */
-		clearCookies: function() {
+		clearCookies: function(name,options = {}) {
+			this.setCookies(name, '', -1, options);
+		},
+		/**
+		 * 获取本地缓存
+		 * @return {[type]} [description]
+		 */
+		getLocalStorage: function() {
+
+		},
+		/**
+		 * 设置本地缓存(可设置过期时间)
+		 * @param {[type]} key   	键
+		 * @param {[type]} value 	值
+		 * @param {[type]} expires  过期时间
+		 */
+		setLocalStorage: function(key, value, expires) {
+
+		},
+		/**
+		 * 清除本地缓存
+		 * @return {[type]} [description]
+		 */
+		clearLocalStorage: function() {
 
 		}
 
